ebd478f2ef90955c806f51edd805af877b0fc2dc
ineaw <ineaw@users.noreply.github.com>
2021-04-18 20:57:58 +0200

    is it

--- js/product.js
+++ js/product.js
@@ -8,12 +8,11 @@ const params = new URLSearchParams(queryString);
 
 const id = params.get("id");
 
-const url = "https://ineaw.no/rainydays/wp-json/wc/store/products/" + id;
+const api = "https://ineaw.no/rainydays/wp-json/wc/v3/products/" + id;
 
-// const key = "consumer_key=ck_805849d86188f7b6115b02b0f46312c46a13276f";
-// const secret = "consumer_secret=cs_9d27e84f1790c49225ddd52f201a93dc7b59cc63";
-
-// const url = `${api}?${key}&${secret}&${id} `;
+const key = "consumer_key=ck_805849d86188f7b6115b02b0f46312c46a13276f";
+const secret = "consumer_secret=cs_9d27e84f1790c49225ddd52f201a93dc7b59cc63";
+const url = `${api}?${key}&${secret}&${id} `;
 
 async function fetchProducts() {
   try {
